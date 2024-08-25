9961b7c0279256dceb61ae94cfd9026c6974c34c
Alejandra Contreras <aleandreaconb@gmail.com>
2024-08-25 00:39:58 -0400

    Cambios en js

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+const textArea = document.querySelector(".area-texto");
+let letra []= "abcdefghijklmnñopqrstuvwxyz";
+letra = prompt;
+
+if (letra [] === "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"){
+    console.log("¡Ingrese solo letras minúsculas!");
+} 
+    else{
+        if (letra ==="e"){
+            console.log("enter");
+
+        } if (letra ==="i"){
+            console.log("imes");
+        } else if(letra ==="a"){
+            console.log("ai");
+        } else if(letra ==="o"){
+            console.log("ober");
+        } else (letra ==="u"){
+            console.log("ufat");
+        }  
+    }
